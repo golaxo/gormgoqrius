499282aa6ca923f587e37106a2f80e46daa455bf
manuelarte <manueldoncelmartos@gmail.com>
2025-12-13 23:04:40 +0100

    chore: updating goqrius version v0.0.4

--- examples/user.go
+++ examples/user.go
@@ -3,12 +3,13 @@ package examples
 import "fmt"
 
 type User struct {
-	ID      int    `json:"id"`
-	Name    string `json:"name"`
-	Surname string `json:"surname"`
-	Age     int    `json:"age"`
+	ID      int     `json:"id"`
+	Name    string  `json:"name"`
+	Surname string  `json:"surname"`
+	Age     int     `json:"age"`
+	Email   *string `json:"email"`
 }
 
 func (u User) String() string {
-	return fmt.Sprintf("%d: %s %s, %d years old", u.ID, u.Name, u.Surname, u.Age)
+	return fmt.Sprintf("%d: %s %s, %d years old, email: %v", u.ID, u.Name, u.Surname, u.Age, u.Email)
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/golaxo/gormgoqrius
 go 1.24.0
 
 require (
-	github.com/golaxo/goqrius v0.0.2
+	github.com/golaxo/goqrius v0.0.4
 	gorm.io/driver/sqlite v1.6.0
 	gorm.io/gorm v1.31.1
 )
@@ -12,5 +12,5 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.32 // indirect
-	golang.org/x/text v0.30.0 // indirect
+	golang.org/x/text v0.32.0 // indirect
 )
